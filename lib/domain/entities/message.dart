69a3f5475ee814c12442f585c7f85e2668131054
E1230 <embeltrans@correo.udistrital.edu.co>
2023-05-10 12:13:57 -0500

    entidad mensaje

---
+++ lib/domain/entities/message.dart
@@ -0,0 +1,9 @@
+enum FromWho { my, user }
+
+class Message {
+  final String message;
+  final String? imageUrl;
+  final FromWho fromWho;
+
+  Message({required this.message, this.imageUrl, required this.fromWho});
+}
