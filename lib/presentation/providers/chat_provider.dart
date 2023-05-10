be64d1235ee95b26b9b161d333a09f364663dd5e
E1230 <embeltrans@correo.udistrital.edu.co>
2023-05-10 15:40:29 -0500

    scroll screen message

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,15 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_maybe_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final chatScrollController = ScrollController();
   List<Message> messages = [
     Message(message: 'Hola', fromWho: FromWho.my),
     Message(message: 'Uwu', fromWho: FromWho.my)
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
     final newMessage = Message(message: text, fromWho: FromWho.my);
     messages.add(newMessage);
-
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut);
   }
 }
